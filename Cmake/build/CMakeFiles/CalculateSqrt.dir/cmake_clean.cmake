39eaf17038e8c96d700c9563a477a8ad830ff71d
levi_personal_cloud <qw991122@126.com>
2022-04-24 11:07:09 +0800

    Add: add cmake note

---
+++ Cmake/build/CMakeFiles/CalculateSqrt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/CalculateSqrt.dir/calculatesqrt.cpp.o"
+  "CalculateSqrt.pdb"
+  "CalculateSqrt"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/CalculateSqrt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
